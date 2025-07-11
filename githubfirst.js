43336f7b31147bebcfacfa135b21112d5b4e19ec
Sakshi Yadav <yadavsakshi6388@gmail.com>
2025-07-11 15:02:45 +0530

    upi added

--- githubfirst.js
+++ githubfirst.js
@@ -10,4 +10,10 @@ console.log(footer);
 =======
 const payment="Integrated payment gateway"
 console.log(payment);
+<<<<<<< HEAD
 >>>>>>> 2a92d97 (payment added)
+=======
+
+const upi="integrated the upi also";
+console.log(upi)
+>>>>>>> 70afef1 (upi added)
