fc8b9475c4e9130e3200af456c2cc754a5db4038
Sakshi Yadav <yadavsakshi6388@gmail.com>
2025-07-11 15:00:49 +0530

    payment update after footer

--- githubfirst.js
+++ githubfirst.js
@@ -4,5 +4,10 @@ console.log(button)
 const login="login page added";
 console.log(login)
 
+<<<<<<< HEAD
 const footer="footer added in our website";
 console.log(footer);
+=======
+const payment="Integrated payment gateway"
+console.log(payment);
+>>>>>>> 2a92d97 (payment added)
