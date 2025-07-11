f496b470d136a90101a1d1099a93cd71e0a5e02b
Sakshi Yadav <yadavsakshi6388@gmail.com>
2025-07-11 15:30:46 +0530

    jabardasti

--- githubfirst.js
+++ githubfirst.js
@@ -4,16 +4,10 @@ console.log(button)
 const login="login page added";
 console.log(login)
 
-<<<<<<< HEAD
 const footer="footer added in our website";
 console.log(footer);
-=======
 const payment="Integrated payment gateway"
 console.log(payment);
-<<<<<<< HEAD
->>>>>>> 2a92d97 (payment added)
-=======
-
 const upi="integrated the upi also";
 console.log(upi)
->>>>>>> 70afef1 (upi added)
+
